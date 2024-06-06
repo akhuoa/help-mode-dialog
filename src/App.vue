3b08c8798a5958c8f51e4a15825e8848aedf6904
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2024-06-06 16:47:33 +1200

    Replace HelpModeDialog component content

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,42 @@
 <script setup>
 import HelpModeDialog from './components/HelpModeDialog.vue';
+
+defineProps({
+  helpMode: {
+    type: Boolean,
+    default: true,
+  },
+  helpModeActiveItem: {
+    type: Number,
+    default: 0
+  },
+  helpModeLastItem: {
+    type: Boolean,
+    default: false
+  },
+  useHelpModeDialog: {
+    type: Boolean,
+    default: true
+  },
+  multiflatmapRef: {
+    type: Object,
+    default: null
+  },
+});
+
+const onHelpModeShowNext = () => console.log('Show Next Item')
+const onFinishHelpMode = () => console.log('Help Mode Finish!')
 </script>
 
 <template>
-  <HelpModeDialog title="Vue Component Template" />
+  <HelpModeDialog
+    v-if="helpMode && useHelpModeDialog"
+    ref="multiflatmapHelp"
+    :multiflatmapRef="multiflatmapRef"
+    :lastItem="helpModeLastItem"
+    @show-next="onHelpModeShowNext"
+    @finish-help-mode="onFinishHelpMode"
+  />
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue';
-import './assets/styles.scss';
 import App from './App.vue';
 
 createApp(App).mount('#app');
