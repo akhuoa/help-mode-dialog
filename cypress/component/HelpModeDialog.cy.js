693b84d7ab396216ba14a7b56ad818f570dd13c9
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2024-06-06 17:27:28 +1200

    Update component testing

--- cypress/component/HelpModeDialog.cy.js
+++ cypress/component/HelpModeDialog.cy.js
@@ -5,4 +5,34 @@ describe('<HelpModeDialog />', () => {
     // see: https://on.cypress.io/mounting-vue
     cy.mount(HelpModeDialog)
   })
+
+  it('clicking Next button should emit show-next event', () => {
+    const onShowNext = cy.spy().as('onShowNext')
+    cy.mount(HelpModeDialog, {
+      props: {
+        showNext: onShowNext,
+      }
+    })
+    .then(({ wrapper, component }) => {
+      cy.get('.el-button:not(.secondary)').click()
+      .then(() => {
+        expect(wrapper.emitted('show-next')).to.have.length(1)
+      })
+    })
+  })
+
+  it('clicking Finish button should emit finish-help-mode event', () => {
+    const onFinishHelpMode = cy.spy().as('onFinishHelpMode')
+    cy.mount(HelpModeDialog, {
+      props: {
+        finishHelpMode: onFinishHelpMode,
+      }
+    })
+    .then(({ wrapper, component }) => {
+      cy.get('.el-button.secondary').click()
+      .then(() => {
+        expect(wrapper.emitted('finish-help-mode')).to.have.length(1)
+      })
+    })
+  })
 })
